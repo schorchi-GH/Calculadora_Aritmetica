b9ae62a1fb1b21688a7865b22c980906e3cc36e6
Georges <georgeshutschinski@gmail.com>
2024-01-23 01:50:47 -0300

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <CalculadoraAritmetica />
+  </div>
+</template>
+
+<script>
+import CalculadoraAritmetica from './components/CalculadoraAritmetica.vue';
+
+export default {
+  components: {
+    CalculadoraAritmetica
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', 'Helvetica', 'Arial', sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: linear-gradient(135deg, #6dd5ed, #ff758c); /* Gradiente de fundo */
+  min-height: 100vh; /* Altura total da viewport */
+}
+
+input, select {
+  font-size: 22px; /* Tamanho da fonte maior */
+  margin: 15px; /* Espaço entre os elementos */
+  padding: 15px; /* Espaçamento interno */
+  border-radius: 8px; /* Borda arredondada */
+  border: none; /* Sem borda */
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Sombra para dar profundidade */
+  background-color: rgba(255, 255, 255, 0.9); /* Cor de fundo com transparência */
+}
+
+select {
+  font-size: 30px; /* 1.5 vezes o tamanho da fonte original */
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+
+p {
+  font-size: 30px; /* Tamanho maior da fonte */
+  color: #ffffff; /* Cor branca para contraste */
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.5); /* Fundo escuro com transparência */
+}
+
+p:hover {
+  transform: scale(1.3);
+  transition: transform 0.3s ease-in-out;
+  background-color: rgba(0, 0, 0, 0.7); /* Fundo mais escuro ao passar o mouse */
+}
+
+button {
+  font-size: 20px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  background-color: #4cd137;
+  color: white;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s, box-shadow 0.3s;
+}
+
+button:hover {
+  background-color: #44bd32;
+  box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.3);
+}
+
+h1 {
+  color: #34495e;
+  font-size: 36px;
+  margin-bottom: 10px;
+}
+
+h3 {
+  color: #0a585e8a;
+  font-size: 20px;
+  margin-bottom: 20px;
+  text-align: left;
+  padding-left: 5%;
+}
+
+</style>
+
+
---
+++ src/components/CalculadoraAritmetica.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Calculadora Aritmética</h1>
+        <h3>- Para utilizar a calculadora, comece inserindo o primeiro valor numérico no campo indicado.</h3>
+        <h3>- Em seguida, selecione a operação desejada – soma, subtração, multiplicação ou divisão – a partir do menu suspenso.</h3>
+        <h3>- Após escolher a operação, digite o segundo número no próximo campo. Assim que ambos os números e a operação estiverem definidos, a calculadora processará automaticamente e exibirá o resultado da operação escolhida.</h3>
+
+        <input type="number" v-model.number="numero1">
+        <select v-model="operacao">
+            <option value="adicao">+</option>
+            <option value="subtracao">-</option>
+            <option value="multiplicacao">*</option>
+            <option value="divisao">/</option>
+        </select>
+        <input type="number" v-model.number="numero2">
+
+        <p>Resultado: {{ resultado }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numero1: 0,
+            numero2: 0,
+            operacao: 'adicao',
+        };
+    },
+    computed: {
+        resultado() {
+            switch (this.operacao) {
+                case 'adicao':
+                    return this.numero1 + this.numero2;
+                case 'subtracao':
+                    return this.numero1 - this.numero2;
+                case 'multiplicacao':
+                    return this.numero1 * this.numero2;
+                case 'divisao':
+                    return this.numero2 !== 0 ? this.numero1 / this.numero2 : 'Divisão por zero!';
+                default:
+                    return 0;
+            }
+        },
+    },
+};
+</script>
